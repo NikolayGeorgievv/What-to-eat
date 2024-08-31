61e5ff04718aae7bf992a19cf8d6291cc664835f
NikolayGeorgievv <skullronx@gmail.com>
2024-08-31 16:01:51 +0300

    Better visualization of the elements. Translated to bulgarian.

--- src/main/java/whattoeat/app/controller/MainPageController.java
+++ src/main/java/whattoeat/app/controller/MainPageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainPageController {
 
 
-    @GetMapping("/")
+    @GetMapping({"/", "/mainPage"})
     private String getMainPage() {
         return "mainPage";
     }
