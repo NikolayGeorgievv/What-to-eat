17117b186381be412f1dee8ebaa9b129860e0569
NikolayGeorgievv <skullronx@gmail.com>
2024-09-09 15:45:16 +0300

    Login page styling. Display login and register button depending on whether the user is logged in.

--- src/main/java/whattoeat/app/controller/MainPageController.java
+++ src/main/java/whattoeat/app/controller/MainPageController.java
@@ -1,11 +1,21 @@
 package whattoeat.app.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class MainPageController {
 
+
+    @ModelAttribute("userEmail")
+    public String userEmail() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        return authentication.getName();
+    }
 
     @GetMapping({"/", "/mainPage"})
     private String getMainPage() {
