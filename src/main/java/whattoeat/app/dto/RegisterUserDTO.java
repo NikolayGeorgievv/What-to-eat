4fef0c8b662e6a4d051fe4a5cece470a00d491ad
NikolayGeorgievv <skullronx@gmail.com>
2025-03-29 12:53:20 +0200

    Added a password policy for registering.

--- src/main/java/whattoeat/app/dto/RegisterUserDTO.java
+++ src/main/java/whattoeat/app/dto/RegisterUserDTO.java
@@ -2,6 +2,7 @@ package whattoeat.app.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class RegisterUserDTO {
@@ -9,8 +10,12 @@ public class RegisterUserDTO {
     @Email
     @Size(max = 40)
     private String email;
+
+
     @NotEmpty(message = "Please enter a password.")
-    @Size(min = 4, max = 40)
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$#!%^*?&])[A-Za-z\\d@$#!%^*?&]{8,40}$",
+            message = "Password must contain at least one uppercase letter, one lowercase letter, one digit, one special character and be between 8 and 40 characters long.")
     private String password;
     @NotEmpty
     private String confirmPassword;
